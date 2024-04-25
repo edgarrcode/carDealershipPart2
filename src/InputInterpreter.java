287bdd22f8f1b99090bcf922aaaac12873eb6b42
Edgar Rodriguez <1983eerr@gmail.com>
2024-04-24 21:33:14 -0600

    first commit

---
+++ src/InputInterpreter.java
@@ -0,0 +1,361 @@
+/**
+* This class has the methods to purchase a car.
+*/
+import java.io.File;
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class InputInterpreter {
+    //logger
+    private static final Logger logger = Logger.getLogger(InputInterpreter.class.getName());
+    private static Car car;
+
+    /**
+     * this method checks if there are cars available to purchase
+     * @param car
+     * @return
+     */
+    public boolean isCarAvailable(Car car){
+        logger.info("Checking car availability");
+        return car.getCarsAvailable() > 0;
+    }
+
+    /**
+     * This method checks if user has enough money to purchase car
+     * @param carCost
+     * @param userMoney
+     * @return
+     */
+    public boolean enoughFunds(double totalCost, double userMoney){
+        logger.info("Checking if user has enough funds");
+        return userMoney >= totalCost;
+    }
+
+    /**
+     * This method uses a switch/case to generate the car object based on the "Car Type"
+     * At the moment it seems a bit redundant, but I was not able to think of better way to input all
+     * necessary data automatically
+     * @param carToBuyData
+     * @return
+     */
+    public Car createCar(String[][] carToBuyData,String[][] data,Finder f){
+        if (carToBuyData.length == 0 || carToBuyData[0].length == 0) {
+            throw new IllegalArgumentException("carToBuyData cannot be empty");
+        }
+
+        String carType = carToBuyData[0][1].trim();
+        String[] rowContents = new String[15];
+
+        System.arraycopy(carToBuyData[0], 0, rowContents, 0, carToBuyData[0].length);
+
+
+        logger.info("Creating car type");
+        //depending on the car type a new car object is created
+         switch (carType) {
+            case "Hatchback" -> {
+                Car car=hatchbackBuilder(rowContents,f,data);
+                return car;
+            }
+            case "Sedan" -> {
+                Car car = sedanBuilder(rowContents,f,data);
+                return car;
+            }
+            case "SUV" -> {
+                Car car = SUVBuilder(rowContents,f,data);
+                return car;
+            }
+            case "Pickup" -> {
+                Car car = pickupBuilder(rowContents,f,data);
+                return car;
+            }
+            default -> throw new IllegalArgumentException("Invalid car type: " + carToBuyData[0][1]);
+
+        }
+
+    }
+
+    /**
+     * This method "purchases" the car by subtracting User money and car availability
+     * as well as adding cars purchased to the user
+     * @param car
+     * @param user
+     * @return
+     */
+    public String[] purchaseCar(Car car, Person user){
+        String username = user.getUsername();
+        double taxRate = 0.0625;
+        String isMember = user.isMembership();
+        double basePrice = car.getPrice();
+        System.out.println(isMember);
+        // Apply a 10% discount if the user is a member
+        if (isMember != null && isMember.equals("TRUE")) {
+            logger.info("User is a member, giving user a 10% discount!");
+            basePrice *= 0.9; // Reduce price by 10%
+        }
+
+        // Calculate total cost including tax
+        double totalCost = basePrice * (1 + taxRate);
+
+        String[] newData = new String[12];
+        if(isCarAvailable(car) && enoughFunds(totalCost, user.getMoney())){
+            logger.info("Car purchased successfuly");
+            newData[0]=Integer.toString(car.getCarsAvailable()-1);
+            newData[1]=Integer.toString(Integer.parseInt(user.getCarsBought())+1);
+            user.setMoney(user.getMoney() - totalCost);
+            newData[2]=Double.toString(user.getMoney());
+            newData[3]=car.getColor();
+            newData[4]=car.getCarType();
+            newData[5]=car.getVin();
+            
+            // Append car details to the "purchased.csv" file
+            appendCarDetailsToPurchasedFile(car, username, totalCost);
+        }
+        else {
+            logger.info("Purchase failed, not enough money.");
+            System.out.println("Not enough money, needed including tax: " + totalCost);
+            System.out.println("Car cost: " + car.getPrice());
+            System.out.println("Tax amount: " + (car.getPrice() * taxRate));
+            System.out.println("Total cost including tax: " + totalCost);
+            System.out.println("Returning to menu");
+            return newData;
+        }
+        return newData;
+    }
+
+    /**
+     * Appends all relevant car details to the "purchased.csv" file.
+     * @param car
+     */
+    private void appendCarDetailsToPurchasedFile(Car car, String username, double totalCost) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("purchased.csv", true))) {
+            // Assuming Car class has getters for each field
+            String carDetails = String.join(",",
+                car.getId(),
+                car.getCarType(),
+                car.getModel(),
+                car.getColor(),
+                car.getCapacity(),
+                car.getFuelType(),
+                car.getTransmission(),
+                car.getVin(),
+                String.valueOf(totalCost),
+                String.valueOf(car.getCarsAvailable()),
+                username
+            );
+
+            bw.write(carDetails + "\n");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to write to purchased.csv", e);
+        }
+    }
+
+    public String loginInfo(String[] loginInfo,int x){
+        return loginInfo[x];
+    }
+
+    /**
+     * Checks if provided Username and password exist on a specific array
+     * if both match
+     * user is allowed to continue
+     * @param info
+     * @param userData
+     * @return
+     */
+    public Boolean loginChecker(String[] info, String[][] userData){
+        if (Objects.equals(info[0], userData[0][6]) && Objects.equals(info[1], userData[0][7])){
+            return true;}
+        else{
+            System.out.println("Invalid login");
+            return false;}
+    }
+
+   public Boolean newLoginChecker(String[] info,String[][] newScrambledUserData,Finder finder){
+        int userNameIndex=finder.findColumnIndex(newScrambledUserData,"Username");
+        int passWordIndex=finder.findColumnIndex(newScrambledUserData,"Password");
+
+        String[] userNames=finder.getColumnValues(newScrambledUserData,userNameIndex);
+        String[] passWord=finder.getColumnValues(newScrambledUserData,passWordIndex);
+
+        if (finder.findUserInputInTheNewColumn(info,userNames,0) &&
+                finder.findUserInputInTheNewColumn(info,passWord,1) ){
+            return true;
+        }
+
+       System.out.println("Invalid login");
+        return false;
+    }
+    public Person personBuilder(String [] rowContentsForPersonValues, Finder f,String[][] data){
+
+        return new Customer(f.valueFinderInRow("ID",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("First Name",rowContentsForPersonValues,data)[0]+" "+f.valueFinderInRow("Last Name",rowContentsForPersonValues,data)[0],
+                Double.parseDouble(f.valueFinderInRow("Money Available",rowContentsForPersonValues,data)[0]),
+                f.valueFinderInRow("Cars Purchased",rowContentsForPersonValues,data)[0],
+                "false",
+                f.valueFinderInRow("Username",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Password",rowContentsForPersonValues,data)[0]
+                );
+    }
+    public Sedan sedanBuilder(String [] rowContentsForPersonValues, Finder f,String[][] data){
+
+        return new Sedan(f.valueFinderInRow("ID",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Car Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Model",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Condition",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Color",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Capacity",rowContentsForPersonValues,data)[0],"0",
+                f.valueFinderInRow("Fuel Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Transmission",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Vin",rowContentsForPersonValues,data)[0],
+                Double.parseDouble(f.valueFinderInRow("Price",rowContentsForPersonValues,data)[0]),
+                Integer.parseInt(f.valueFinderInRow("Cars Available",rowContentsForPersonValues,data)[0]));
+
+    }
+    public SUV SUVBuilder(String [] rowContentsForPersonValues, Finder f,String[][] data){
+
+        return new SUV(f.valueFinderInRow("ID",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Car Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Model",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Condition",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Color",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Capacity",rowContentsForPersonValues,data)[0],
+                "0",
+                f.valueFinderInRow("Fuel Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Transmission",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Vin",rowContentsForPersonValues,data)[0],
+                Double.parseDouble(f.valueFinderInRow("Price",rowContentsForPersonValues,data)[0]),
+                Integer.parseInt(f.valueFinderInRow("Cars Available",rowContentsForPersonValues,data)[0]));
+
+    }
+    public Hatchback hatchbackBuilder(String [] rowContentsForPersonValues, Finder f,String[][] data){
+
+        return new Hatchback(f.valueFinderInRow("ID",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Car Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Model",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Condition",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Color",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Capacity",rowContentsForPersonValues,data)[0],"0",
+                f.valueFinderInRow("Fuel Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Transmission",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Vin",rowContentsForPersonValues,data)[0],
+                Double.parseDouble(f.valueFinderInRow("Price",rowContentsForPersonValues,data)[0]),
+                Integer.parseInt(f.valueFinderInRow("Cars Available",rowContentsForPersonValues,data)[0]));
+
+    }
+    public Pickup pickupBuilder(String [] rowContentsForPersonValues, Finder f,String[][] data){
+
+        return new Pickup(f.valueFinderInRow("ID",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Car Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Model",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Condition",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Color",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Capacity",rowContentsForPersonValues,data)[0],
+                "0",
+                f.valueFinderInRow("Fuel Type",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Transmission",rowContentsForPersonValues,data)[0],
+                f.valueFinderInRow("Vin",rowContentsForPersonValues,data)[0],
+                Double.parseDouble(f.valueFinderInRow("Price",rowContentsForPersonValues,data)[0]),
+                Integer.parseInt(f.valueFinderInRow("Cars Available",rowContentsForPersonValues,data)[0]));
+
+    }
+
+
+    /**
+     * Main logic handler
+     * Depending on user input several different processes take place
+     * * Side-note: Most errors and difficulties came from this method
+     * it can definitely be improved
+     * @param menuInput
+     * @param fileReader2
+     * @param printer
+     * @param data
+     * @param scanner
+     * @param user
+     * @return
+     */
+    public String[] menuChoice(int menuInput, FileReader2 fileReader2,
+                           Printer printer, String[][] data, Scanner scanner, Person user, Finder f){
+        String[] newDataToSendToExcelFile = new String[6];
+
+        switch (menuInput){
+            case 1:
+            logger.info("Menu Option: 1. Display all cars.");
+            printer.printALLData(data);
+
+            break;
+            case 2:
+            logger.info("Menu Option: 2. Filter Cars (used / new).");
+                printer.printFilterCars();
+                int x=Integer.parseInt(scanner.nextLine());
+                if (x==1) {
+                    logger.info("Menu Option: 2. Filter Cars (used / new)> New.");
+                    String condition = "new";
+                   printer.printCarData(fileReader2.filterDataByCondition(data, condition, 3));
+                break;
+                }
+                if(x==2){
+                    logger.info("Menu Option: 2. Filter Cars (used / new)> Used.");
+                    String condition = "used";
+                    printer.printCarData(fileReader2.filterDataByCondition(data, condition, 3));
+                    break;
+                }
+                if(x==3){
+                    logger.info("Menu Option: 2. Filter Cars (used / new)> Back.");
+                    System.out.println("Going back to Main menu");
+                    break;
+                    }
+                else {
+                    logger.info("Menu Option: 2. Filter Cars (used / new)> invalid choice, going back to menu.");
+                    System.out.println("invalid choice, going back to menu");
+
+                }
+                return null;
+
+            case 3:
+                logger.info("Menu Option: 3. Purchase a car");
+                System.out.println("Please provide ID for the car you want to purchase");
+                String condition= scanner.nextLine();
+                String[][] carToBuyData=fileReader2.filterDataByCondition(data,condition,f.findColumnIndex(data,"ID"));
+
+
+
+                Car myNewCar=createCar(carToBuyData,data,f);
+                setCar(myNewCar);
+                newDataToSendToExcelFile=purchaseCar(myNewCar,user);
+                if (newDataToSendToExcelFile[0]==null){
+                    return null;
+                }
+                //Here we have to ask to filter what car they want to buy, we can use car id
+                //here we set car data=filtered car
+                System.out.println("Press 4 for ticket!");
+                condition= scanner.nextLine();
+                if(condition.equals("4")){
+                    System.out.println("You have purchased a "+myNewCar.isCondition()+" " +myNewCar.getColor() +" "+myNewCar.getCarType()+ " VIN: "+myNewCar.getVin());
+                    System.out.println("Your new Balance is: "+user.getMoney());
+                   //testing
+                    // System.out.println(myNewCar.getId());
+                }
+                break;
+            case 4:
+                logger.info("Menu Option: 4. View Tickets");
+                System.out.println("You must buy a car first!");
+                break;
+        }
+        return newDataToSendToExcelFile;
+    }
+    public void setCar(Car car){
+        this.car=car;
+    }
+
+    public static Car getCar() {
+        return car;
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,77 @@
+public class Person implements User {
+
+//public abstract class Person {
+String username;
+String password;
+String fullName;
+double money;
+String id;
+String membership;
+
+String carsBought;
+    public Person(String id, String fullName, double money,String carsBought, String membership,String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.money = money;
+        this.id = id;
+        this.carsBought=carsBought;
+        this.membership = membership;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String isMembership() {
+        return membership;
+    }
+
+    public void setMembership(String membership) {
+        this.membership = membership;
+    }
+
+    public String getCarsBought() {
+        return carsBought;
+    }
+
+    public void setCarsBought(String carsBought) {
+        this.carsBought = carsBought;
+    }
+}
